cd73d744ee3ffcf4e547e5ca2ee441e8b99dfda8
Hasan Abbasi <h.abbasi@citydi.com>
2021-06-30 13:46:45 +0430

    add parcel, react, prettier and eslint and extract out Pet component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
-const Pet =({name, animal, breed})=>{
-    return React.createElement(
-        "div",
-        {id:"sth-important"},
-        [
-            React.createElement(
-                "h1", {}, name
-            ),
-            React.createElement(
-                "h2", {}, animal
-            ),
-            React.createElement(
-                "h3", {}, breed
-            ),
-        ]
-    );
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return React.createElement("div", { id: "sth-important" }, [
+    // can be array or one simple element!
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "magoul",
+      animal: "hamster",
+      breed: "robofski",
+    }),
+    React.createElement(Pet, {
+      name: "magoul",
+      animal: "hamster",
+      breed: "robofski",
+    }),
+    React.createElement(Pet, {
+      name: "magoul",
+      animal: "hamster",
+      breed: "robofski",
+    }),
+  ]);
 };
 
-
-const App =()=>{
-    return React.createElement(
-        "div",
-        {id:"sth-important"},
-        [ // can be array or one simple element!
-            React.createElement(
-                "h1", {}, "Adopt Me!"
-            ),
-            React.createElement(Pet, { name:"magoul", animal:"hamster", breed:"robofski"}),
-            React.createElement(Pet, { name:"magoul", animal:"hamster", breed:"robofski"}),
-            React.createElement(Pet, { name:"magoul", animal:"hamster", breed:"robofski"}),
-        ]
-    );
-};
-
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
