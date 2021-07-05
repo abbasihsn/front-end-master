9baf3b2d92634f8983fe9226d537a2ba076372a9
Hasan Abbasi <h.abbasi@citydi.com>
2021-07-05 14:57:57 +0430

    hooks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParamas from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", { id: "sth-important" }, [
-    // can be array or one simple element!
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "magoul",
-      animal: "hamster",
-      breed: "robofski",
-    }),
-    React.createElement(Pet, {
-      name: "magoul",
-      animal: "hamster",
-      breed: "robofski",
-    }),
-    React.createElement(Pet, {
-      name: "magoul",
-      animal: "hamster",
-      breed: "robofski",
-    }),
-  ]);
+  // return React.createElement("div", { id: "sth-important" }, [
+  //   // can be array or one simple element!
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "magoul",
+  //     animal: "hamster",
+  //     breed: "robofski",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "magoul",
+  //     animal: "hamster",
+  //     breed: "robofski",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "magoul",
+  //     animal: "hamster",
+  //     breed: "robofski",
+  //   }),
+  // ]);
+  return (
+    <div id="sth-important">
+      <h1>Adopt Me!</h1>
+      <SearchParamas />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
---
+++ src/SearchParams.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
+const SearchParamas = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location ">
+          <h2>{location}</h2>
+          Location
+          <input
+            type="text"
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParamas;
